98bcb8562cd86318de5572256ab3586906a23dc7
siam114 <smsiamislam598@gmail.com>
2025-09-24 01:54:20 +0600

    app file use cookie parser and express

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 import express from "express";
 import { config } from "dotenv";
 import cors from "cors"
+import cookieParser from "cookie-parser";
 
 
 const app = express();
 
 config({path: "./config/config.env"});
 
-app.use(cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL]
-}))
+app.use(
+    cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+})
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
