fd219d97e7c1730038f4ca9df08c0a19625a7921
siam114 <smsiamislam598@gmail.com>
2025-09-25 01:12:02 +0600

    create database schema

--- server/database/db.js
+++ server/database/db.js
@@ -1,3 +1,11 @@
 import pkg from "pg";
 
 const {Client} = pkg;
+
+const database = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+})
