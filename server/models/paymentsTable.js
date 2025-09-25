3da9de0d3a8372bd9547ad79149c7b78db6ac54b
siam114 <smsiamislam598@gmail.com>
2025-09-26 00:50:16 +0600

    error solve

--- server/models/paymentsTable.js
+++ server/models/paymentsTable.js
@@ -5,7 +5,7 @@ export async function createPaymentsTable() {
         CREATE TABLE IF NOT EXISTS payments (         
              id UUID DEFAULT gen_random_uuid() PRIMARY KEY,         
              order_id UUID NOT NULL UNIQUE,         
-             payment_type VARCHAR(20) NOT NULL CHECK (payment_type IN ('Online')),         
+             payment_type VARCHAR(20) NOT NULL CHECK (payment_type IN ('Online', 'Cash On Delivery')),         
              payment_status VARCHAR(20) NOT NULL CHECK (payment_status IN ('Paid', 'Pending', 'Failed')),         
              payment_intent_id VARCHAR(255) UNIQUE,         
              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,         
