8be0863aa92903b78e015c4b555bc6f911ee3fc1
siam114 <smsiamislam598@gmail.com>
2025-09-27 00:32:52 +0600

    import creating tables

---
+++ server/utils/createTables.js
@@ -0,0 +1,15 @@
+import {createUserTable} from "../models/userTable"
+import {createOrderItemTable} from "../models/orderItemsTable"
+import {createOrdersTable} from "../models/ordersTable"
+import {createPaymentsTable} from "../models/paymentsTable"
+import {createProductReviewsTable} from "../models/productReviewsTable"
+import {createProductsTable} from "../models/productTable"
+import {createShippingInfoTable} from "../models/shippinginfoTable"
+
+export const createTables = async () => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
