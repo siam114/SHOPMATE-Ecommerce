ec6ffd0a2cb76dcf9e37bb3b5bd26c0af3d7746b
siam114 <smsiamislam598@gmail.com>
2025-09-28 01:57:22 +0600

    create error middleware

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -1,29 +1,40 @@
 class ErrorHandler extends Error {
-    constructor(message, statusCode){
-        super(message);
-        this.statusCode = statusCode;
-    }
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
 }
 
 export const errorMiddleware = (err, req, res, next) => {
-    err.message = err.message || "Internal Server Error";
-    err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
 
-    if(err.code === 11000){
-        const message = "Duplicate field value entered";
-        err = new ErrorHandler(message, 400);
-    }
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    err = new ErrorHandler(message, 400);
+  }
 
-    if(err.name = "JsonWebTokenError"){
-        const message = "JSON Web Token is invalid, try again";
-        err = new ErrorHandler(message, 400);
-    
-    }
+  if ((err.name = "JsonWebTokenError")) {
+    const message = "JSON Web Token is invalid, try again";
+    err = new ErrorHandler(message, 400);
+  }
 
-    if(err.name = "TokenExpiredError"){
-         const message = "JSON Web Token has expired, try again";
-        err = new ErrorHandler(message, 400);
-    }
-}
+  if ((err.name = "TokenExpiredError")) {
+    const message = "JSON Web Token has expired, try again";
+    err = new ErrorHandler(message, 400);
+  }
+  console.log(err);
+
+  const errorMessage = err.errors
+    ? Object.values(err.errors)
+        .map((error) => error.message)
+        .join(" ")
+    : err.message;
+
+  return res.status(err.statusCode).json({
+    success: false,
+    message: errorMessage,
+  })
+};
 
 export default ErrorHandler;
