a39321b30449db2324e761347dd2c0cf09c72a22
siam114 <smsiamislam598@gmail.com>
2025-09-29 02:16:22 +0600

    Please provide all required fields

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,18 @@ import ErrorHandler from "../middlewares/errorMiddleware.js";
 import {catchAsyncErrors} from "../middlewares/catchAsyncError.js";
 import database from '../database/db.js';
 
-export const register = catchAsyncErrors(async(req,res,next) =>{})
+export const register = catchAsyncErrors(async(req,res,next) =>{
+    const {name, email, password} = req.body;
+    if(!name || !email || !password){
+        return next(new ErrorHandler("Please provide all required fields", 400))
+    }
+})
+
+
 export const login = catchAsyncErrors(async(req,res,next) =>{})
+
+
 export const getUser = catchAsyncErrors(async(req,res,next) =>{})
+
+
 export const logout = catchAsyncErrors(async(req,res,next) =>{})
