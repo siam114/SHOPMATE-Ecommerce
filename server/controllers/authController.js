188be882eab05d27efc547af175a697c13bea14e
siam114 <smsiamislam598@gmail.com>
2025-09-29 02:24:58 +0600

    formating

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,27 +1,25 @@
 import ErrorHandler from "../middlewares/errorMiddleware.js";
-import {catchAsyncErrors} from "../middlewares/catchAsyncError.js";
-import database from '../database/db.js';
+import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
+import database from "../database/db.js";
 
-export const register = catchAsyncErrors(async(req,res,next) =>{
-    const {name, email, password} = req.body;
-    if(!name || !email || !password){
-        return next(new ErrorHandler("Please provide all required fields", 400))
-    };
+export const register = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return next(new ErrorHandler("Please provide all required fields", 400));
+  }
 
-    const isAlreadyRegistered = await database.query(
-        `SELECT * FROM users WHERE email = $1`,[email]
-    );
+  const isAlreadyRegistered = await database.query(
+    `SELECT * FROM users WHERE email = $1`,
+    [email]
+  );
 
-    if(isAlreadyRegistered.rows.length > 0){
-        return next(new ErrorHandler("User already registered", 400));
-    };
-})
+  if (isAlreadyRegistered.rows.length > 0) {
+    return next(new ErrorHandler("User already registered", 400));
+  }
+});
 
+export const login = catchAsyncErrors(async (req, res, next) => {});
 
-export const login = catchAsyncErrors(async(req,res,next) =>{})
+export const getUser = catchAsyncErrors(async (req, res, next) => {});
 
-
-export const getUser = catchAsyncErrors(async(req,res,next) =>{})
-
-
-export const logout = catchAsyncErrors(async(req,res,next) =>{})
+export const logout = catchAsyncErrors(async (req, res, next) => {});
