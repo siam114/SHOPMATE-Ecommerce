ae6ea929e245d051c6e2158998f0da8fae401ed6
siam114 <smsiamislam598@gmail.com>
2025-09-29 02:12:41 +0600

    create login user logout function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,3 +3,6 @@ import {catchAsyncErrors} from "../middlewares/catchAsyncError.js";
 import database from '../database/db.js';
 
 export const register = catchAsyncErrors(async(req,res,next) =>{})
+export const login = catchAsyncErrors(async(req,res,next) =>{})
+export const getUser = catchAsyncErrors(async(req,res,next) =>{})
+export const logout = catchAsyncErrors(async(req,res,next) =>{})
