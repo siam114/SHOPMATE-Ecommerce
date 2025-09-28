1abbed1a6328f6092919ff2bd8ddaa76741bcdae
siam114 <smsiamislam598@gmail.com>
2025-09-29 02:10:24 +0600

    create register function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,5 @@
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import {catchAsyncErrors} from "../middlewares/catchAsyncError.js";
 import database from '../database/db.js';
+
+export const register = catchAsyncErrors(async(req,res,next) =>{})
