b15e3fc81f751bb8573748c32ee840a5d839609f
siam114 <smsiamislam598@gmail.com>
2025-09-29 02:24:29 +0600

    user exist

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,11 +6,15 @@ export const register = catchAsyncErrors(async(req,res,next) =>{
     const {name, email, password} = req.body;
     if(!name || !email || !password){
         return next(new ErrorHandler("Please provide all required fields", 400))
-    }
+    };
 
     const isAlreadyRegistered = await database.query(
         `SELECT * FROM users WHERE email = $1`,[email]
-    )
+    );
+
+    if(isAlreadyRegistered.rows.length > 0){
+        return next(new ErrorHandler("User already registered", 400));
+    };
 })
 
 
