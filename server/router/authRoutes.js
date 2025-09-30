7639a18a112effec847d6718692b8b2325d082b5
siam114 <smsiamislam598@gmail.com>
2025-10-01 01:59:35 +0600

    router create

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -1,6 +1,11 @@
 import { express } from 'express';
+import { getUser, login, logout, register } from '../controllers/authController';
 
 const router = express.Router();
 
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout);
+router.get("/me", getUser);
 
 export default router;
