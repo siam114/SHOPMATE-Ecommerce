5771e9ddbdb335692fb1669fc5f920f595e862ea
siam114 <smsiamislam598@gmail.com>
2025-10-01 02:05:51 +0600

    import app file auth router

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { createTables } from "./utils/createTables.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import authRouter from "./router/authRoutes.js"
 
 
 const app = express();
@@ -29,6 +30,8 @@ app.use(
         useTempFiles: true,
     })
 );
+
+app.use("/api/v1/auth", authRouter)
 
 createTables();
 
