f4d986799e71ba8286f41d46a3ece9afe70aa26b
siam114 <smsiamislam598@gmail.com>
2025-10-02 13:23:42 +0600

    check pass

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -27,6 +27,7 @@ export const register = catchAsyncErrors(async (req, res, next) => {
   sendToken(user.rows[0], 201, "User registed successfully", res);
 });
 
+
 export const login = catchAsyncErrors(async (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -40,7 +41,11 @@ export const login = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("Invalid email and password",401));
   }
 
-  const isPasswordMatch = await bcrypt.compare(password, user.rows[0].password)
+  const isPasswordMatch = await bcrypt.compare(password, user.rows[0].password);
+  if(!isPasswordMatch){
+     return next(new ErrorHandler("Invalid email and password",401));
+  };
+  sendToken(user.rows[0], 201, "Logged in successfully", res);
 });
 
 export const getUser = catchAsyncErrors(async (req, res, next) => {});
