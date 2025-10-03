e97007f2eed86f1685e50a424f0f453665fe2a39
siam114 <smsiamislam598@gmail.com>
2025-10-04 00:38:20 +0600

    get user from database

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -4,8 +4,15 @@ import { catchAsyncErrors } from ".catchAsyncError.js";
 import database from "../database/db.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-    const {token} = req.cookies;
-    if(!token){
-        return next(new ErrorHandler("Please login to access this resource",401));
-    }
-})
+  const { token } = req.cookies;
+  if (!token) {
+    return next(new ErrorHandler("Please login to access this resource", 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  const user = await database.query(
+    "SELECT * FROM users WHERE id = $1 LIMIT 1",
+    [decoded.id]
+  );
+});
