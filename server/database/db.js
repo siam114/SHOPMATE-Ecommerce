76e11660a2bc44adb17480fc088c3f624bfd2cda
siam114 <smsiamislam598@gmail.com>
2025-10-03 20:49:59 +0600

    formatting

--- server/database/db.js
+++ server/database/db.js
@@ -1,21 +1,21 @@
 import pkg from "pg";
 
-const {Client} = pkg;
+const { Client } = pkg;
 
 const database = new Client({
-    user: "postgres",
-    host: "localhost",
-    database: "mern_ecommerce_store",
-    password: "114674",
-    port: 5432,
-})
+  user: "postgres",
+  host: "localhost",
+  database: "mern_ecommerce_store",
+  password: "114674",
+  port: 5432,
+});
 
 try {
-    await database.connect();
-    console.log("Connected to the database successfully");
+  await database.connect();
+  console.log("Connected to the database successfully");
 } catch (error) {
-    console.error("Database connection failed", error);
-    process.exit(1);
+  console.error("Database connection failed", error);
+  process.exit(1);
 }
 
 export default database;
