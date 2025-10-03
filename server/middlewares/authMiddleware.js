260f3ceb1b24f4e14c8a4b6ac4a475838d97426b
siam114 <smsiamislam598@gmail.com>
2025-10-04 00:25:00 +0600

    create is authenticated function format

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -2,3 +2,5 @@ import jwt from "jsonwebtoken";
 import ErrorHandler from "./errorMiddleware.js";
 import { catchAsyncErrors } from ".catchAsyncError.js";
 import database from "../database/db.js";
+
+export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {})
