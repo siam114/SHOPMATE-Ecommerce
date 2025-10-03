1e0f25fea23ec1cd793c9db30bb1ce8f8156c3a6
siam114 <smsiamislam598@gmail.com>
2025-10-04 00:30:03 +0600

    check token

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -3,4 +3,9 @@ import ErrorHandler from "./errorMiddleware.js";
 import { catchAsyncErrors } from ".catchAsyncError.js";
 import database from "../database/db.js";
 
-export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {})
+export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+    const {token} = req.cookies;
+    if(!token){
+        return next(new ErrorHandler("Please login to access this resource",401));
+    }
+})
