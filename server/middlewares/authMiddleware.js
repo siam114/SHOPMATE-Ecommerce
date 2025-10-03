a8d2fe3b3208fdd8706c3a269ccaec7bddd20df2
siam114 <smsiamislam598@gmail.com>
2025-10-04 00:47:59 +0600

    formatting

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -19,11 +19,15 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   next();
 });
 
-
 export const authorizeRoles = (...roles) => {
-    return (req, res, next) => {
-        if(!roles.includes(req.user.role)){
-            return next(new ErrorHandler(`Role: ${req.user.role} is not allowed to access this resource.`,403))
-        }
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user.role} is not allowed to access this resource.`,
+          403
+        )
+      );
     }
-}
+  };
+};
