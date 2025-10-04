7bc2ce6b2c5ad20d0b985bc5ccca29e6d20d298a
siam114 <smsiamislam598@gmail.com>
2025-10-05 01:37:38 +0600

    error solve

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import ErrorHandler from "./errorMiddleware.js";
-import { catchAsyncErrors } from ".catchAsyncError.js";
+import { catchAsyncErrors } from "./catchAsyncError.js";
 import database from "../database/db.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
