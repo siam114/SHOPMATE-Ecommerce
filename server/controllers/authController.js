eee26299a4dbfff900c51673feb0ec46dc9be0b3
siam114 <smsiamislam598@gmail.com>
2025-10-05 01:45:10 +0600

    logout message

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -59,5 +59,8 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
   res.status(200).cookie("token", "", {
     expires: new Date(DataTransfer.now()),
     httpOnly: true,
-  })
+  }).json({
+    success: true,
+    message: "Logged out successfully",
+  });
 });
