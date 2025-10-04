ab74ae492942bb97f278811a4144a72c23ba1a79
siam114 <smsiamislam598@gmail.com>
2025-10-05 01:48:23 +0600

    error solve

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -59,7 +59,7 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
   res
     .status(200)
     .cookie("token", "", {
-      expires: new Date(DataTransfer.now()),
+      expires: new Date(Date.now()),
       httpOnly: true,
     })
     .json({
