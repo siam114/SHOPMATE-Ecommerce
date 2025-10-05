7d49965a5e3c4b36fd1bb5f1ddd0d27e90912e87
siam114 <smsiamislam598@gmail.com>
2025-10-06 00:48:49 +0600

    expire time password

--- server/utils/generateResetPasswordToken.js
+++ server/utils/generateResetPasswordToken.js
@@ -1,10 +1,12 @@
 import crypto from "crypto";
 
-const generateResetPasswordToken = () => {
+export const generateResetPasswordToken = () => {
   const resetToken = crypto.randomBytes(20).toString("hex");
 
   const hashedToken = crypto
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
+
+  const resetPasswordExpireTime = Date.now() + 15 * 60 * 1000; // 15 minutes
 };
