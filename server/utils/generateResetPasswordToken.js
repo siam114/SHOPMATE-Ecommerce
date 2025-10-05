b7f3e9829ce57045a6db4f8212b1b4cad60b028b
siam114 <smsiamislam598@gmail.com>
2025-10-06 00:44:54 +0600

    create hashed token

--- server/utils/generateResetPasswordToken.js
+++ server/utils/generateResetPasswordToken.js
@@ -1,3 +1,10 @@
-import crypto from 'crypto';
+import crypto from "crypto";
 
-const generateResetPasswordToken = () => {};
+const generateResetPasswordToken = () => {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+};
