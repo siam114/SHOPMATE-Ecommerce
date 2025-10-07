4e6f7cc91be2b5b1db3fc1f149e421554b903cf3
siam114 <smsiamislam598@gmail.com>
2025-10-07 09:17:06 +0600

    formatting

--- server/utils/createTables.js
+++ server/utils/createTables.js
@@ -1,22 +1,22 @@
-import {createUserTable} from "../models/userTable.js"
-import {createOrderItemTable} from "../models/orderItemsTable.js"
-import {createOrdersTable} from "../models/ordersTable.js"
-import {createPaymentsTable} from "../models/paymentsTable.js"
-import {createProductReviewsTable} from "../models/productReviewsTable.js"
-import {createProductsTable} from "../models/productTable.js"
-import {createShippingInfoTable} from "../models/shippinginfoTable.js"
+import { createUserTable } from "../models/userTable.js";
+import { createOrderItemTable } from "../models/orderItemsTable.js";
+import { createOrdersTable } from "../models/ordersTable.js";
+import { createPaymentsTable } from "../models/paymentsTable.js";
+import { createProductReviewsTable } from "../models/productReviewsTable.js";
+import { createProductsTable } from "../models/productTable.js";
+import { createShippingInfoTable } from "../models/shippinginfoTable.js";
 
 export const createTables = async () => {
-    try {
-        await createUserTable();
-        await createProductsTable();
-        await createOrdersTable();
-        await createOrderItemTable();
-        await createPaymentsTable();
-        await createProductReviewsTable();
-        await createShippingInfoTable();
-        console.log("All Tables create successfully");
-    } catch (error) {
-        console.log("Error Creating Tables", error);
-    }
-}
+  try {
+    await createUserTable();
+    await createProductsTable();
+    await createOrdersTable();
+    await createOrderItemTable();
+    await createPaymentsTable();
+    await createProductReviewsTable();
+    await createShippingInfoTable();
+    console.log("All Tables create successfully");
+  } catch (error) {
+    console.log("Error Creating Tables", error);
+  }
+};
