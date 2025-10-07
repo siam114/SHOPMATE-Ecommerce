da6e71e3c83ecd858a1d762c9262c0a7cc5ea45b
siam114 <smsiamislam598@gmail.com>
2025-10-08 01:15:01 +0600

    create transporter

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,5 +1,13 @@
 import nodeMailer from "nodemailer";
 
 export const sendEmail = async ({email, subject, message})=> {
-    
+    const transporter = nodeMailer.createTransport({
+        host: process.env.SMTP_HOST,
+        service: process.env.SMTP_SERVICE,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_MAIL,
+            pass: process.env.SMTP_PASSWORD
+        },
+    })
 }
