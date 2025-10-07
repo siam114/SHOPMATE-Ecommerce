e9fcf9a3fc257d4bbcd636b3cb9b9baed0c70401
siam114 <smsiamislam598@gmail.com>
2025-10-08 01:16:45 +0600

    create mail options

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -9,5 +9,12 @@ export const sendEmail = async ({email, subject, message})=> {
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD
         },
-    })
+    });
+
+    const mailOptions = {
+        from: process.env.SMTP_MAIL,
+        to: email,
+        subject,
+        html: message,
+    }
 }
