81436fba027cd015da1e9997584525b443c79d19
siam114 <smsiamislam598@gmail.com>
2025-10-11 19:41:16 +0600

    create post route

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  forgotPassword,
   getUser,
   login,
   logout,
@@ -13,5 +14,6 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/me", isAuthenticated, getUser);
 router.get("/logout", isAuthenticated, logout);
+router.post("/password/forgot", forgotPassword);
 
 export default router;
