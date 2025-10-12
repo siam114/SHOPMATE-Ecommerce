c0bf850794cff907940028ff486832bc6bad275a
siam114 <smsiamislam598@gmail.com>
2025-10-12 13:56:00 +0600

    formatting

--- server/app.js
+++ server/app.js
@@ -1,37 +1,36 @@
 import express from "express";
 import { config } from "dotenv";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { createTables } from "./utils/createTables.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-import authRouter from "./router/authRoutes.js"
-
+import authRouter from "./router/authRoutes.js";
 
 const app = express();
 
-config({path: "./config/config.env"});
+config({ path: "./config/config.env" });
 
 app.use(
-    cors({
+  cors({
     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-})
+  })
 );
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    fileUpload({
-        tempFileDir: "./uploads",
-        useTempFiles: true,
-    })
+  fileUpload({
+    tempFileDir: "./uploads",
+    useTempFiles: true,
+  })
 );
 
-app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/auth", authRouter);
 
 createTables();
 
