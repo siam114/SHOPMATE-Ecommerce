8ecdceb194f480124ccf6b098113702b4f58f933
siam114 <smsiamislam598@gmail.com>
2025-10-22 00:38:11 +0600

    error did not find

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,6 +1,7 @@
 import nodeMailer from "nodemailer";
 
 export const sendEmail = async ({ email, subject, message }) => {
+  
   const transporter = nodeMailer.createTransport({
     host: process.env.SMTP_HOST,
     service: process.env.SMTP_SERVICE,
