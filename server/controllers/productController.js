92b415b077e427e1571d8c6511d9c807fa50d591
siam114 <smsiamislam598@gmail.com>
2025-10-26 00:48:25 +0600

    import cloudinary

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,7 +1,9 @@
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import database from "../database/db.js";
+import { v2 as cloudinary } from "cloudinary";
 
 export const createProduct = catchAsyncErrors(async (req, res, next) => {
-    
+  const { name, description, price, category, stock } = req.body;
+  const created_by = req.user.id;
 })
