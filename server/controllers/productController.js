8be27adf31649c22c4230c1ff708241cf5c8ed9f
siam114 <smsiamislam598@gmail.com>
2025-10-26 00:27:45 +0600

    create product function

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,3 +1,7 @@
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import database from "../database/db.js";
+
+export const createProduct = catchAsyncErrors(async (req, res, next) => {
+    
+})
