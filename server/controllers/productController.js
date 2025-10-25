5e0cffafe518317a2a33136c079b8576423991aa
siam114 <smsiamislam598@gmail.com>
2025-10-26 00:53:00 +0600

    formatting

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -7,16 +7,16 @@ export const createProduct = catchAsyncErrors(async (req, res, next) => {
   const { name, description, price, category, stock } = req.body;
   const created_by = req.user.id;
 
-    if (!name || !description || !price || !category || !stock) {
+  if (!name || !description || !price || !category || !stock) {
     return next(
       new ErrorHandler("Please provide complete product details.", 400)
     );
   }
 
   let uploadedImages = [];
-    if (req.files && req.files.images) {
-          const images = Array.isArray(req.files.images)
+  if (req.files && req.files.images) {
+    const images = Array.isArray(req.files.images)
       ? req.files.images
       : [req.files.images];
-    }
-})
+  }
+});
