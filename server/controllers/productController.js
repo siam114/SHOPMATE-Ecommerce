d07a78403490617f47a8eab96149e46b64e56ecc
siam114 <smsiamislam598@gmail.com>
2025-10-26 00:49:36 +0600

    check product details

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -6,4 +6,10 @@ import { v2 as cloudinary } from "cloudinary";
 export const createProduct = catchAsyncErrors(async (req, res, next) => {
   const { name, description, price, category, stock } = req.body;
   const created_by = req.user.id;
+
+    if (!name || !description || !price || !category || !stock) {
+    return next(
+      new ErrorHandler("Please provide complete product details.", 400)
+    );
+  }
 })
