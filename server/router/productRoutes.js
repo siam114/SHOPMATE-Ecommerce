84c49f5bcd9450125a8890a254f5c2a3bb5a8b64
siam114 <smsiamislam598@gmail.com>
2025-10-27 00:19:00 +0600

    export router

--- server/router/productRoutes.js
+++ server/router/productRoutes.js
@@ -13,3 +13,23 @@ router.post(
   authorizedRoles("Admin"),
   createProduct
 );
+
+// router.get("/", fetchAllProducts);
+// router.get("/singleProduct/:productId", fetchSingleProduct);
+// router.put("/post-new/review/:productId", isAuthenticated, postProductReview);
+// router.delete("/delete/review/:productId", isAuthenticated, deleteReview);
+// router.put(
+//   "/admin/update/:productId",
+//   isAuthenticated,
+//   authorizedRoles("Admin"),
+//   updateProduct
+// );
+// router.delete(
+//   "/admin/delete/:productId",
+//   isAuthenticated,
+//   authorizedRoles("Admin"),
+//   deleteProduct
+// );
+// router.post("/ai-search", isAuthenticated, fetchAIFilteredProducts);
+
+export default router;
