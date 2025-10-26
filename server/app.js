a7ba33ab7762392d90a0e9d1a23c7ecf71677d6f
siam114 <smsiamislam598@gmail.com>
2025-10-27 00:27:30 +0600

    set up product route

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ import fileUpload from "express-fileupload";
 import { createTables } from "./utils/createTables.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import authRouter from "./router/authRoutes.js";
+import productRouter from "./router/productRoutes.js";
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use(
 );
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/product", productRouter);
 
 createTables();
 
